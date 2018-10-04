7b81db948e5df6d096b2cdfe22e6ed1925235079
Ali Haider <alihaidder_pilot@hotmail.com>
2018-10-04 19:26:56 +1000

    Code refactoring and removing duplicate code

--- TestProject/FunctionalTesting.cs
+++ TestProject/FunctionalTesting.cs
@@ -9,146 +9,93 @@ namespace TestProject
     [TestClass]
     public class FunctionalTesting
     {
+        private readonly DbContextOptions<EntityContext> _options;
+        private readonly EntityContext _context;
+        private readonly EntityData _entityData;
+
+        public FunctionalTesting()
+        {
+            _options = new DbContextOptionsBuilder<EntityContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new EntityContext(_options);
+            _entityData = new EntityData(_context);
+        }
+
         [TestMethod]
         public void Exception_On_Stored_Procedure_Call_InMemoryDB()
         {
-            var options = new DbContextOptionsBuilder<EntityContext>()
-                .UseInMemoryDatabase("DummyDB")
-                .Options;
-
-            using (var context = new EntityContext(options))
-            {
-                var entityData = new EntityData(context);
-                Assert.ThrowsException<NotSupportedException>(() => entityData.Get("A"));
-            }
+            Assert.ThrowsException<NotSupportedException>(() => _entityData.Get("A"));
         }
 
         [TestMethod]
         public void Fetching_Data_Without_Stored_Procedure_Call_InMemoryDB()
         {
-            var options = new DbContextOptionsBuilder<EntityContext>()
-                .UseInMemoryDatabase("DummyDB1")
-                .Options;
-
-            using (var context = new EntityContext(options))
-            {
-                var entityData = new EntityData(context);
-                var res = entityData.GetData("A");
-
-                Assert.AreEqual(2, res.Count);
-            }
+            var res = _entityData.GetData("A");
         }
 
         [TestMethod]
         public void Fetching_Data_For_Type_B()
         {
-            var options = new DbContextOptionsBuilder<EntityContext>()
-                .UseInMemoryDatabase("DummyDB2")
-                .Options;
+            var res = _entityData.GetData("B");
 
-            using (var context = new EntityContext(options))
-            {
-                var entityData = new EntityData(context);
-                var res = entityData.GetData("B");
-
-                Assert.AreEqual(2, res.Count);
-            }
+            Assert.AreEqual(2, res.Count);
         }
 
         [TestMethod]
         public void Fetching_Data_For_Type_C()
         {
-            var options = new DbContextOptionsBuilder<EntityContext>()
-                .UseInMemoryDatabase("DummyDB3")
-                .Options;
+            var res = _entityData.GetData("C");
 
-            using (var context = new EntityContext(options))
-            {
-                var entityData = new EntityData(context);
-                var res = entityData.GetData("C");
-
-                Assert.AreEqual(3, res.Count);
-            }
+            Assert.AreEqual(3, res.Count);
         }
 
         [TestMethod]
         public void Fetching_Data_For_Type_D()
         {
-            var options = new DbContextOptionsBuilder<EntityContext>()
-                .UseInMemoryDatabase("DummyDB4")
-                .Options;
+            var res = _entityData.GetData("D");
 
-            using (var context = new EntityContext(options))
-            {
-                var entityData = new EntityData(context);
-                var res = entityData.GetData("D");
-
-                Assert.AreEqual(1, res.Count);
-            }
+            Assert.AreEqual(1, res.Count);
         }
 
         [TestMethod]
         public void Fetching_Data_For_Type_E()
         {
-            var options = new DbContextOptionsBuilder<EntityContext>()
-                .UseInMemoryDatabase("DummyDB5")
-                .Options;
+            var res = _entityData.GetData("E");
 
-            using (var context = new EntityContext(options))
-            {
-                var entityData = new EntityData(context);
-                var res = entityData.GetData("E");
-
-                Assert.AreEqual(0, res.Count);
-            }
+            Assert.AreEqual(0, res.Count);
         }
 
         [TestMethod]
         public void Add_Data_For_Type_E()
         {
-            var options = new DbContextOptionsBuilder<EntityContext>()
-                .UseInMemoryDatabase("DummyDB6")
-                .Options;
-
-            using (var context = new EntityContext(options))
+            _context.Entities.Add(new EntityLibrary.Entities.Entity()
             {
-                var entityData = new EntityData(context);
-                context.Entities.Add(new EntityLibrary.Entities.Entity()
-                {
-                    Id = 10,
-                    Type = "E",
-                    Content = "This is test data for type E with ID 10",
-                    Created = DateTime.Now
-                });
-                context.SaveChanges();
+                Id = 10,
+                Type = "E",
+                Content = "This is test data for type E with ID 10",
+                Created = DateTime.Now
+            });
+            _context.SaveChanges();
 
-                // Get Data for Type E
-                var res = entityData.GetData("E");
+            // Get Data for Type E
+            var res = _entityData.GetData("E");
 
-                Assert.AreEqual(1, res.Count);
-            }
+            Assert.AreEqual(1, res.Count);
         }
 
         [TestMethod]
         public void Remove_Data_For_Type_A()
         {
-            var options = new DbContextOptionsBuilder<EntityContext>()
-                .UseInMemoryDatabase("DummyDB7")
-                .Options;
+            var entA = _entityData.GetData("A");
+            _context.Entities.RemoveRange(entA);
+            _context.SaveChanges();
 
-            using (var context = new EntityContext(options))
-            {
-                var entityData = new EntityData(context);
+            // Get Data for Type A
+            var res = _entityData.GetData("A");
 
-                var entA = entityData.GetData("A");
-                context.Entities.RemoveRange(entA);
-                context.SaveChanges();
-
-                // Get Data for Type A
-                var res = entityData.GetData("A");
-
-                Assert.AreEqual(0, res.Count);
-            }
+            Assert.AreEqual(0, res.Count);
         }
     }
 }
